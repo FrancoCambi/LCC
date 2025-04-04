29523e5282537dc8573e5e4aaba297c6d3e8b98b
Franco <francoocambiaso@gmail.com>
2025-04-04 15:54:27 -0300

    p2 ej9 no recursivo

--- EDyAII/EDyAII_P2/ej9.hs
+++ EDyAII/EDyAII_P2/ej9.hs
@@ -3,5 +3,7 @@ zip3_ [] l2 l3 = []
 zip3_ l1 [] l3 = []
 zip3_ l1 l2 [] = []
 zip3_ (x1:xs1) (x2:xs2) (x3:xs3) = [(x1, x2, x3)] ++ zip3_ xs1 xs2 xs3
-
+ 
+zip3nr  :: [a] -> [b] -> [c] -> [(a, b, c)]
+zip3nr l1 l2 l3 = (map (\ ((x, y), z) -> (x, y, z)) (zip (zip l1 l2) l3))
 
